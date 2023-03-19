c068afa53f111ceacf9128df73e6b951cfd24dca
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-19 13:30:03 -0500

    created function and wrote logic to donate to campaign in donateToCampaign in the web3campaign.sol smart contract file

--- web3campaigns/contracts/web3campaign.sol
+++ web3campaigns/contracts/web3campaign.sol
@@ -37,7 +37,20 @@ contract web3campaign {
         return numberOfCampaigns -1;
     }
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        unit256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+        // Variable to let us know if the transaction has been sent or not.
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonators() {}
     
