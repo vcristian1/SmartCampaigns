ac82cb0e2bf47b0bd1667c0720f05491092930d0
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-19 15:13:30 -0500

    imported index.css in main.jsx to resolve error with tailwind classes not rendering any styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         Sidebar
       </div>
       <div className='flex-1 max-sm:w-full max-w[1280px] mx-auto sm:pr-5'>
-      Navbar
+        Navbar
       </div>
     </div>
   )
