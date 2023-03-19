91d50e653fcf334e76423d100bc74f4f7d17e3d4
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-19 15:19:26 -0500

    created four pages in pages directory for Home, Profile, CreateCampaign, CampaignDetails and imported them in App.jsx so we can use them in the Route accordingly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages"
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
@@ -9,6 +9,10 @@ const App = () => {
       </div>
       <div className='flex-1 max-sm:w-full max-w[1280px] mx-auto sm:pr-5'>
         Navbar
+
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
       </div>
     </div>
   )
