2d0df77aa119cb54701a26029dfae109526f81b4
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-19 14:07:44 -0500

    added defaultNetwork and networks to include goerli in the project

--- web3campaigns/hardhat.config.js
+++ web3campaigns/hardhat.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        // This is the account we will use to deploy our smart contract
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
