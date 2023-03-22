dec15001f9f0c86ca01a9e12e336bfc8cd8e9618
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-22 13:43:08 -0500

    prematurely closed the bracket in line 54 which did not allow the mobile menu to open or close. Added the closing bracket after translate-y-0 which resolved the error

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
            onClick={() => setToggleDrawer(!toggleDrawer)}
            />
 
-           <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer} ? '-translate-y-[100vh]' :  'translate-y-0' transition-all duration-700`}>
+           <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' :  'translate-y-0'} transition-all duration-700`}>
             <ul className='mb-4'>
               {navlinks.map((link) => (
                 <li
