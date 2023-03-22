7d42aeb08434a8ad81289726295a84242206c3d4
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-22 13:39:43 -0500

    added the custom button component in the code for the small screens only so it is shown in the mobile menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -70,6 +70,19 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
+
+            <div className='flex mx-4'>
+              <CustomButton 
+                btnType="button" 
+                title={address ? 'Create a campaign' :
+                'Connect'}
+                styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+                handleClick={() => {
+                  if (address) navigate('create-campaign')
+                  else 'connect()'
+                }}
+              />
+            </div>
            </div>
       </div>
     </div>
