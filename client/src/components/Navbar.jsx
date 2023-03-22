a817b2ef4ba9026791de2340ce20deecdb823438
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-22 09:27:10 -0500

    added custom button and create a campaign button in navbar.tsx, also added styling to CustomButton component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
+  const address = '0xabc';
 
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
@@ -16,6 +17,24 @@ const Navbar = () => {
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
         </div>
+      </div>
+
+      <div className='sm:flex hidden flex-row justify-end gap-4'>
+        <CustomButton 
+         btnType="button" 
+         title={address ? 'Create a campaign' :
+        'Connect'}
+         styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+         handleClick={() => {
+          if (address) navigate('create-campaign')
+          else 'connect()'
+         }}
+        />
+        <Link to="/profile">
+          <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+            <img src={thirdweb} alt="third-web-icon" className='w-[60%] h-[60%] object-contain'/>
+          </div>
+        </Link>
       </div>
     </div>
   )
