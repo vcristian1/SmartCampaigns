e7970152bfd7702b7b7cbb49833e8a977282ea0f
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-22 09:34:56 -0500

    added mobile menu burger within small screens nav container so it is only shown on small screens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -36,7 +36,14 @@ const Navbar = () => {
             <img src={thirdweb} alt="third-web-icon" className='w-[60%] h-[60%] object-contain'/>
           </div>
         </Link>
+      </div>
 
+      {/* Small Screen Nav Here */}
+      <div className='sm:hidden flex justify-between items-center relative'>
+          <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+            <img src={thirdweb} alt="third-web-icon" className='w-[60%] h-[60%] object-contain'/>
+          </div>
+          <img src={menu} alt="menu" className='w-[34px] h-[34px] object-contain cursor-pointer' />
       </div>
     </div>
   )
