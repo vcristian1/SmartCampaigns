21af7ed3f8ddc2375802394d582708a590882f4e
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-23 08:16:43 -0500

    added imports to create campaign, added state for the form that will be featured in create campaign page

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    title: '',
+    description: '',
+    target: '',
+    deadline: '',
+    image: '',
+  });
+
   return (
-    <div>CreateCampaign</div>
+    <div className='bg-[#]'>CreateCampaign</div>
   )
 }
 
